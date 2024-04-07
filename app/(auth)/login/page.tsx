633f7a39c6fb39295382b16187096d8f67d11af0
karan <karanverma201411@gmail.com>
2024-04-07 14:47:41 +0530

    route groups

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+    return (
+        <h1>This is a login page</h1>
+    )
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+export default function Register() {
+    return (
+        <h1>This is a register page</h1>
+    )
+}
