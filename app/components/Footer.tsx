dd13e9112af2a75c822a4aba0b10f32551af379c
karan <karanverma201411@gmail.com>
2024-04-09 23:03:19 +0530

    layout file

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <header style={{ backgroundColor: 'yellow', padding: '1rem' }}>
+            <p>Footer</p>
+        </header>
+    )
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header style={{ backgroundColor: 'yellow', padding: '1rem' }}>
+            <p>Header</p>
+        </header>
+    )
+}
